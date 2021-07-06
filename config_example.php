015ef3d9777857716a8fc817ff1097d3abfa1c54
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-07-07 00:59:55 +0200

    config

--- config_example.php
+++ config_example.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'api' => [
+        'url' => 'https://api.com',
+        'token' => ''
+    ],
     'db' => [
         'host' => '',
         'database' => 'channel_storage',
