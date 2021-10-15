7fbdee5a7b31cb7daa387152422bdcfe6285c5b0
Pierre MINIGGIO <pierre.miniggio@new-immo-group.com>
2021-10-15 13:47:48 +0200

    update psr implementation

--- src/Logger.php
+++ src/Logger.php
@@ -4,51 +4,52 @@ namespace PierreMiniggio\TiktokToShorts;
 
 use Exception;
 use Psr\Log\LoggerInterface;
+use Stringable;
 
 class Logger implements LoggerInterface
 {
 
-    public function emergency($message, array $context = [])
+    public function emergency(string | Stringable $message, array $context = []): void
     {
         throw new Exception('Emergency : ' . $message . ' Context :' . json_encode($context));
     }
 
-    public function alert($message, array $context = [])
+    public function alert(string | Stringable $message, array $context = []): void
     {
         $this->log('alert', $message, $context);
     }
 
-    public function critical($message, array $context = [])
+    public function critical(string | Stringable $message, array $context = []): void
     {
         $this->log('critical', $message, $context);
     }
 
-    public function error($message, array $context = [])
+    public function error(string | Stringable $message, array $context = []): void
     {
         $this->log('error', $message, $context);
     }
 
-    public function warning($message, array $context = [])
+    public function warning(string | Stringable $message, array $context = []): void
     {
         $this->log('warning', $message, $context);
     }
 
-    public function notice($message, array $context = [])
+    public function notice(string | Stringable $message, array $context = []): void
     {
         $this->log('notice', $message, $context);
     }
 
-    public function info($message, array $context = [])
+    public function info(string | Stringable $message, array $context = []): void
     {
         $this->log('info', $message, $context);
     }
 
-    public function debug($message, array $context = [])
+    public function debug(string | Stringable $message, array $context = []): void
     {
         $this->log('debug', $message, $context);
     }
 
-    public function log($level, $message, array $context = [])
+    public function log($level, string | Stringable $message, array $context = []): void
     {
         var_dump($level, $message, $context);
     }
