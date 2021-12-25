96c2e3a97c2511e04ae9fd1da61c000ff519e763
Pierre Miniggio <pierre.miniggio@gmail.com>
2021-12-25 22:45:18 +0100

    remove unallowed chars on Youtube

--- src/App.php
+++ src/App.php
@@ -87,7 +87,7 @@ class App
             foreach ($videosToPost as $videoToPost) {
                 $defaultLegend = 'Most Awesome Shorts Video Ever #bestshorts';
                 $legend = $videoToPost['legend'] ?? $defaultLegend;
-                $legend = str_replace(['<', '>'], '', $legend); // Remove unallowed chars on Youtube
+                $legend = str_replace(['<', '>'], ' ', $legend); // Remove unallowed chars on Youtube
 
                 echo PHP_EOL . 'Posting ' . $legend . ' ...';
 
